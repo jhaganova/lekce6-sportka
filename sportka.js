f9e784e1da7530e8e5e1f5e58dc1f91351a6fd7a
Jana Haganová <jhaganova@gmail.com>
2022-03-22 19:58:33 +0100

    button draws numbers

--- sportka.js
+++ sportka.js
@@ -1,24 +1,31 @@
 // SPORTKA
 
-let osudi = [];
-let tazenaCisla = [];
 let vyherniCisla = document.querySelector('#vyherni-cisla');
 
-// generovani osudi
-for (let i = 1; i <= 49; i = i + 1) {
-    osudi.push(i);
+
+function drawNumbers() {
+    vyherniCisla.innerHTML = "";
+    let osudi = [];
+    let tazenaCisla = [];
+
+    // generovani osudi
+    for (let i = 1; i <= 49; i = i + 1) {
+        osudi.push(i);
+    }
+
+    // z osudi vyberu 7 nahodnych cisel
+    for (let i = 0; i < 7; i++) {
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
+        
+        tazenaCisla.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+    }
+
+    // vypis do HTML
+    for (let j = 0; j < tazenaCisla.length; j++) {
+        vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+    }
+
 }
 
-// z osudi vyberu 7 nahodnych cisel
-for (let i = 0; i < 7; i++) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
-    
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
-}
-
-// vypis do HTML
-for (let j = 0; j < tazenaCisla.length; j++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
-}
