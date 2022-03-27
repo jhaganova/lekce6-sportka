4067eb1e3580d4c753d7d09f0c7f4f78d86c5c74
Jana Haganová <jhaganova@gmail.com>
2022-03-27 15:47:27 +0200

    timeout draw

--- sportka.js
+++ sportka.js
@@ -1,12 +1,14 @@
 // SPORTKA
 
 let vyherniCisla = document.querySelector('#vyherni-cisla');
-
+let historieCisel = document.querySelector('#historie-losovani');
+let runningTimeouts = [];
 
 function drawNumbers() {
     vyherniCisla.innerHTML = "";
     let osudi = [];
     let tazenaCisla = [];
+    clearAllRunningTimeouts();
 
     // generovani osudi
     for (let i = 1; i <= 49; i = i + 1) {
@@ -16,22 +18,44 @@ function drawNumbers() {
     // z osudi vyberu 7 nahodnych cisel
     for (let i = 0; i < 7; i++) {
 
-        setTimeout(function() {
-            drawOneNumber(osudi, tazenaCisla, i)}, 2000*i);
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
+
+        tazenaCisla.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+
+        let timeoutid = setTimeout(function() {
+            drawOneNumber(osudi, tazenaCisla, i)}, 500*i);
+
+        runningTimeouts.push(timeoutid);
 
     }
 
- 
+    historieCisel.innerHTML += '<p class="historicke-losovani">' + tazenaCisla.join(' ') + '</p>';
+
+
 }
 
-function drawOneNumber(osudi, tazenaCisla, i) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
 
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
+function drawOneNumber(osudi, tazenaCisla, i) {
 
     // vypis do HTML
     vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[i] + '</span>';
 }
 
+function clearAllRunningTimeouts() {
+    for (let i = 0; i < runningTimeouts.length; i++) {
+        let timeoutid = runningTimeouts[i];
+        clearTimeout(timeoutid);
+    }
+
+    runningTimeouts = [];
+
+}
+
+
+
+
+
+// history saves too early
+// make more delay
